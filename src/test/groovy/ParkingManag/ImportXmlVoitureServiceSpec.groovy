e17016cae766bc16ee16ec02f8fdc39ba46d839c
Sire Bah <bahsire1@gmail.com>
2025-05-13 10:27:44 +0000

    add service to import data from an xml file with unit tests

---
+++ src/test/groovy/ParkingManag/ImportXmlVoitureServiceSpec.groovy
@@ -0,0 +1,12 @@
+package ParkingManag
+
+import grails.testing.services.ServiceUnitTest
+import spock.lang.Specification
+
+class ImportXmlVoitureServiceSpec extends Specification implements ServiceUnitTest<ImportXmlVoitureService> {
+
+     void "test something"() {
+        expect:
+        service.doSomething()
+     }
+}
